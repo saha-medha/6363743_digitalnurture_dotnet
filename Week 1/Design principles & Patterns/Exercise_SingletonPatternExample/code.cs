e7e8d57c6bbb5eff10841e33e115d032f09ed7a3
Medha Saha <22053079@kiit.ac.in>
2025-06-22 13:02:40 +0530

    Add files via upload

---
+++ Week 1/Design principles & Patterns/Exercise_SingletonPatternExample/code.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Logger
+{
+    private static Logger _instance;
+    private static readonly object _lock = new object();
+
+    private Logger()
+    {
+        Console.WriteLine("Logger initialized.");
+    }
+
+    public static Logger GetInstance()
+    {
+        lock (_lock)
+        {
+            if (_instance == null)
+            {
+                _instance = new Logger();
+            }
+        }
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("[LOG] " + message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("First message");
+        logger2.Log("Second message");
+
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Both logger instances are the same.");
+        }
+        else
+        {
+            Console.WriteLine("Logger instances are different!");
+        }
+    }
+}
