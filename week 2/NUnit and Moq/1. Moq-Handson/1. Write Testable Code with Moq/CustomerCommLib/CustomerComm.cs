61be28f0b7fc66e3a640e0eb384d689070cf58f5
Medha Saha <22053079@kiit.ac.in>
2025-06-29 21:10:11 +0530

    Update CustomerComm.cs

--- week 2/NUnit and Moq/1. Moq-Handson/1. Write Testable Code with Moq/CustomerCommLib/CustomerComm.cs
+++ week 2/NUnit and Moq/1. Moq-Handson/1. Write Testable Code with Moq/CustomerCommLib/CustomerComm.cs
@@ -1,1 +1,17 @@
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
 
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            return _mailSender.SendMail("sahamedha1315.com", "HI");
+        }
+    }
+}
