6b568695f112d14553ba41ab6a8a1971644f439f
Medha Saha <22053079@kiit.ac.in>
2025-07-06 13:53:07 +0530

    Update Program.cs

--- week 3/EF Core 8.0 HOL/Lab 3/RetailInventoryLab/Code/Program.cs
+++ week 3/EF Core 8.0 HOL/Lab 3/RetailInventoryLab/Code/Program.cs
@@ -1,1 +1,10 @@
+using (var context = new AppDbContext())
+{
+    var products = context.Products.ToList();
+
+    foreach (var product in products)
+    {
+        Console.WriteLine($"Product: {product.Name}, Price: {product.Price}, Category ID: {product.CategoryId}");
+    }
+}
 
