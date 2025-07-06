df658971ea402422de6945432c6750f760e4d482
Medha Saha <22053079@kiit.ac.in>
2025-07-06 13:10:01 +0530

    Update Program.cs

--- week 3/EF Core 8.0 HOL/Lab 2/Program.cs
+++ week 3/EF Core 8.0 HOL/Lab 2/Program.cs
@@ -6,17 +6,12 @@ class Program
     {
         using (var context = new AppDbContext())
         {
-            // New category add karo
             var electronics = new Category { Name = "Electronics" };
             context.Categories.Add(electronics);
             context.SaveChanges();
-
-            // Ek product bhi add karo
             var phone = new Product { Name = "Smartphone", Price = 25000, CategoryId = electronics.Id };
             context.Products.Add(phone);
             context.SaveChanges();
-
-            // Print karo
             foreach (var product in context.Products)
             {
                 Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
