d4e0cd17bf8a610e5b328d8a5154aa4da8157433
Medha Saha <22053079@kiit.ac.in>
2025-07-06 13:54:13 +0530

    Create AppDbContext.cs

---
+++ week 3/EF Core 8.0 HOL/Lab 3/RetailInventoryLab/Code/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    optionsBuilder.UseSqlServer(
+    "Server=localhost\\SQLEXPRESS;Database=RetailDB;Trusted_Connection=True;Encrypt=False;"
+);
+    }
+}
