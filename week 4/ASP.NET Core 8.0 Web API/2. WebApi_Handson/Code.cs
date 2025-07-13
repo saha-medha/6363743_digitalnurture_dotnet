fb168f1094739f88d9dc5e9adbf4666b70c722b2
Medha Saha <22053079@kiit.ac.in>
2025-07-13 22:05:31 +0530

    Update Code.cs

--- week 4/ASP.NET Core 8.0 Web API/2. WebApi_Handson/Code.cs
+++ week 4/ASP.NET Core 8.0 Web API/2. WebApi_Handson/Code.cs
@@ -1,1 +1,38 @@
-
+using Microsoft.OpenApi.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Swagger Demo",
+        Version = "v1",
+        Description = "Simple Employee Web API with Swagger",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "John Doe",
+            Email = "john@example.com",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo");
+    });
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
